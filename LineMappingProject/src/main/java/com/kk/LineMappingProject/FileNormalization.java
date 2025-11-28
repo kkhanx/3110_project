d18ae7a08eb77676e1551efc8b72905ba305524e
kkhanx <kulsumk2005@gmail.com>
2025-11-28 16:52:42 -0500

    initial commit of FileNormalization.java [kulsum]

---
+++ LineMappingProject/src/main/java/com/kk/LineMappingProject/FileNormalization.java
@@ -0,0 +1,12 @@
+package com.kk.LineMappingProject;
+//kulsum khan - 110139964
+
+public class FileNormalization {
+	
+	//class will be used to turn code files into standardized text files for processing
+	
+	/*to do: remove whitepsaces around words, standardize spaces to 1 space (all indents and spaces between
+	 * words must become 1 space), make everything lower case, ensure encoding is UTF-8
+	*/
+
+}
