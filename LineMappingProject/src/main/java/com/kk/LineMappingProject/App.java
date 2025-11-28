374652fdd74b9ace66a9bd1c1e33e9dc3165e420
kkhanx <kulsumk2005@gmail.com>
2025-11-28 11:41:13 -0500

    initial commit of maven project [kulsum]

---
+++ LineMappingProject/src/main/java/com/kk/LineMappingProject/App.java
@@ -0,0 +1,15 @@
+package com.kk.LineMappingProject;
+
+/**
+ * Hello world!
+ *
+ */
+
+//testing the project
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
